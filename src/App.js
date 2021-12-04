1cc212ff40df41e0f1e9c9f4c799c4bfd9b68a7e
error707-persona <areeshasayed09@gmail.com>
2021-12-04 17:38:09 +0530

    init-3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import logo from './logo.svg';
+import React, {Component} from 'react'
+import { Route, Link, Routes } from 'react-router-dom';
+import {Layout, Typography, Space} from 'antd';
 import './App.css';
-
+import {Button, Menu, Avatar} from 'antd';
+import {api} from './components/api'
+import { Navbar, Exchanges, HomePage, CryptoDetails, Cryptocurrencies, News } from './components';
+const { Header, Content, Footer, Sider } = Layout;
 function App() {
+//   var axios = require("axios").default;
+// var options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/stats',
+//   headers: {
+//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//     'x-rapidapi-key': 'f17a429a90msh542db698e4d2b60p1c2e9djsn9726c9727048'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+    <Navbar/>
+    <Layout className="site-layout" style={{ marginLeft: 200 }}>
+      <Header className="site-layout-background" style={{ padding: 0 }} />
+      <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+        <div className="site-layout-background" style={{ padding: 24 }}>
+        
+             <Routes>
+             <Route exact path="/" element={<HomePage/>}/>
+               <Route exact path="/exchanges" element={<Exchanges/>}/>
+               <Route exact path="/cryptocurrencies"  element={<Cryptocurrencies/>}/>
+               <Route exact path="/crypto/:coinId" element={<CryptoDetails/>}/>
+               <Route exact path="/news" element={<News/>}/>
+             </Routes>
+            <api/>
+           {/* </div> */}
+        </div>
+      </Content>
+      <div className="footer">
+      
+ <Typography.Title level={5} style={{color: 'white', textAlign:'center'}}>
+   Crypton<br/>
+   All Rights Reserved
+ </Typography.Title>
+ <Space>
+   <Link to="/">Home</Link>
+   <Link to="/cryptocurrencies">Currencies</Link>
+   <Link to="/exchanges">Exchanges</Link>
+   <Link to="/news">News</Link>
+ </Space>
+      </div>
+    </Layout>
+  </Layout>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'antd/dist/antd.css'
 import reportWebVitals from './reportWebVitals';
-
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from'./app/store';
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
+      <Provider store={store}>
     <App />
+    </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
